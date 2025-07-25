341b2294eff5e17d566be66ffa0482529288b650
daffaalex22 <daffaalex22@gmail.com>
2025-07-25 14:57:10 +0700

    feat: add login form component with test credential functionality

---
+++ src/components/login-form.tsx
@@ -0,0 +1,88 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import React, { useRef } from "react"
+import { toast } from "sonner"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
+  const handleTestCredential = () => {
+    const testEmail = "akun_test_fe"
+    const testPassword = "mY)5cl37|iY{"
+    if (
+      emailRef.current?.value === testEmail &&
+      passwordRef.current?.value === testPassword
+    ) {
+      toast.info("The form is already filled with the test credential.")
+      return
+    }
+    if (emailRef.current) emailRef.current.value = testEmail
+    if (passwordRef.current) passwordRef.current.value = testPassword
+  }
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  ref={emailRef}
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  ref={passwordRef}
+                />
+              </div>
+              <Button
+                type="button"
+                variant="secondary"
+                className="w-full cursor-pointer"
+                onClick={handleTestCredential}
+              >
+                Use test credential
+              </Button>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full cursor-pointer">
+                  Login
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
